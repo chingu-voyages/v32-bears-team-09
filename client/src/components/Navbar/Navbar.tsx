acb06531a46e642528f6b59c4e8d4f21a5d78162
aaronmivo <aarmivo93@gmail.com>
2021-07-27 14:45:36 -0700

    Added basic search button

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 
 import './Navbar.css';
 
 const Navbar = () => {
+    const [search, setSearch] = useState("")
+
+    const handleSubmit = (e:any) => {
+        e.preventDefault()
+        //pass search to redux state
+        console.log(search)
+        setSearch("")
+    }
+
     return (
         <nav>
            <ul className="nav-links">
                 <Link to='/'>
                     <li>Homepage</li>
                 </Link>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}/>
+                    <button type="submit">Submit</button>
+                </form>
                 <Link to='/playlists'>
                     <li>Playlists</li>
                 </Link>
