2999578814fa8257a0f36c87a40142a5568c6b47
Ken Cruz <ken@kencruz.ca>
2021-08-04 23:54:15 -0400

    implemented frontend spotify token fetch

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //Components
-import Navbar from '../Navbar/Navbar';
-import Player from '../Player/Player';
+import Navbar from "../Navbar/Navbar";
+import Player from "../Player/Player";
 
 //Pages
-import Homepage from '../../pages/Homepage/Homepage'
-import Playlists from '../../pages/Playlists/Playlists'
-import FriendList from '../../pages/FriendList/FriendList'
-import Friend from '../../pages/Friend/Friend'
-import FeedPage from '../../pages/FeedPage/FeedPage'
-import LoginRegister from '../../pages/LoginRegister/LoginRegister'
+import Homepage from "../../pages/Homepage/Homepage";
+import Playlists from "../../pages/Playlists/Playlists";
+import FriendList from "../../pages/FriendList/FriendList";
+import Friend from "../../pages/Friend/Friend";
+import FeedPage from "../../pages/FeedPage/FeedPage";
+import LoginRegister from "../../pages/LoginRegister/LoginRegister";
+import SpotifyAuth from "../../pages/SpotifyAuth/SpotifyAuth";
 
 function App() {
   return (
     <Router>
       <div className="App">
         {console.log("App page")}
-        <Navbar/>
+        <Navbar />
         <Switch>
-          <Route exact path="/"  component={Homepage}/>
-          <Route path="/playlists" component={Playlists}/>
-          <Route exact path="/friendlist" component={FriendList}/>
-          <Route path ="/activityfeed" component={FeedPage} />
-          <Route path="/shop/:id" component={Friend}/>
-          <Route path="/login" component={LoginRegister}/>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/playlists" component={Playlists} />
+          <Route exact path="/friendlist" component={FriendList} />
+          <Route path="/activityfeed" component={FeedPage} />
+          <Route path="/shop/:id" component={Friend} />
+          <Route path="/login" component={LoginRegister} />
+          <Route path="/spotify_auth" component={SpotifyAuth} />
         </Switch>
         <div className="MediaPlayer">
           <Player />
---
+++ client/src/pages/SpotifyAuth/SpotifyAuth.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Redirect } from "react-router";
+import axios, { AxiosRequestConfig } from "axios";
+
+const SpotifyAuth = ({ location }: { location: Record<any, string> }) => {
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const code = urlParams.get("code");
+    const data = JSON.stringify({ code: code });
+    const config: AxiosRequestConfig = {
+      method: "post",
+      url: process.env.REACT_APP_BACKEND_API + "spotify_token",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+    axios(config)
+      .then((response) => {
+        localStorage.setItem("spotify_token", response.data.access_token);
+        setToken(response.data.access_token);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [location.search]);
+
+  if (!token) {
+    return <div>Receiving token... </div>;
+  } else {
+    localStorage.setItem("spotifysocial_token", token);
+    return <Redirect to="/" />;
+  }
+};
+
+export default SpotifyAuth;
