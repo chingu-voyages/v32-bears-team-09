7b7ae4ae7c0ce65fcb7c2925a5d69862c7ed9183
Ken Cruz <ken@kencruz.ca>
2021-08-07 23:25:45 -0400

    implemented spotify-web-api-node

--- server/routes/api/spotify.js
+++ server/routes/api/spotify.js
@@ -1,38 +1,29 @@
-const axios = require("axios");
-const qs = require("qs");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const fetch_spotify_token = (req, res) => {
-  const { code } = req.body;
-
-  const clientIDClientSecretEncode = btoa(
-    process.env.SPOTIFY_CLIENT_ID + ":" + process.env.SPOTIFY_CLIENT_SECRET
-  );
-
-  const data = qs.stringify({
-    code: code,
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-    grant_type: "authorization_code",
-  });
-
-  const config = {
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
-    headers: {
-      Authorization: "Basic " + clientIDClientSecretEncode,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    data: data,
+  const credentials = {
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_REDIRECT_URI,
   };
 
-  axios(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-      res.status(200).send(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).send(error);
-    });
+  const { code } = req.body;
+
+  const spotifyApi = new SpotifyWebApi(credentials);
+
+  spotifyApi.authorizationCodeGrant(code).then(
+    (data) => {
+      console.log("The token expires in " + data.body["expires_in"]);
+      console.log("The access token is " + data.body["access_token"]);
+      console.log("The refresh token is " + data.body["refresh_token"]);
+      const access_token = data.body["access_token"];
+      res.status(200).send({ access_token });
+    },
+    (err) => {
+      console.log("Something went wrong!", err);
+      res.status(400).send(err);
+    }
+  );
 };
 
 exports.fetch_spotify_token = fetch_spotify_token;
